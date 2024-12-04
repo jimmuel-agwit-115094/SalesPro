73f2499b361403729a42ed5a8d6ad14006caaea7
jagwit <115094@nelnet.net>
2024-12-04 15:00:21 +0800

    fixed login ui

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -144,6 +144,7 @@
             this.Controls.Add(this.password_tx);
             this.Controls.Add(this.username_tx);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
