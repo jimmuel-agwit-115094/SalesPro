01e8560f4a1fbed47c82cb05f67dae3458bb85d2
jagwit <115094@nelnet.net>
2024-12-10 22:05:50 +0800

    added

--- Accessors/Accessor.cs
+++ Accessors/Accessor.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using POS_Generic.Helpers;
-using SalesPro.Helpers;
 using SalesPro.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace SalesPro.Accessors
 {
