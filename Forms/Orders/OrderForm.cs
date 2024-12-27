3e5c332c759c0186d17c13444314201a1917ba48
jagwit <115094@nelnet.net>
2024-12-27 08:46:33 +0800

    added orders

---
+++ Forms/Orders/OrderForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesPro.Forms.Order
+{
+    public partial class Order : Form
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void SetFormSize()
+        {
+            WindowState = FormWindowState.Maximized;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Size = workingArea.Size;
+            Location = workingArea.Location;
+        }
+        private void Order_Load(object sender, EventArgs e)
+        {
+            SetFormSize();
+        }
+    }
+}
