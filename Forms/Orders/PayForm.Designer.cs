d6bde41deb39c478c2a23cbd5da9f857d88db5cd
jagwit <115094@nelnet.net>
2025-01-01 11:11:09 +0800

    added

--- Forms/Orders/PayForm.Designer.cs
+++ Forms/Orders/PayForm.Designer.cs
@@ -38,22 +38,25 @@
             this.product_tx = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.discRate_tx = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.discAmt_tx = new System.Windows.Forms.NumericUpDown();
+            this.change_tx = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.cash_tx)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discRate_tx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discAmt_tx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.change_tx)).BeginInit();
             this.SuspendLayout();
             // 
             // customer_tx
             // 
             this.customer_tx.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.customer_tx.Location = new System.Drawing.Point(29, 197);
+            this.customer_tx.Location = new System.Drawing.Point(29, 163);
             this.customer_tx.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.customer_tx.Name = "customer_tx";
             this.customer_tx.Size = new System.Drawing.Size(387, 23);
@@ -64,7 +67,7 @@
             // amtDue_tx
             // 
             this.amtDue_tx.Font = new System.Drawing.Font("Segoe UI Semibold", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.amtDue_tx.Location = new System.Drawing.Point(29, 133);
+            this.amtDue_tx.Location = new System.Drawing.Point(29, 99);
             this.amtDue_tx.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.amtDue_tx.Name = "amtDue_tx";
             this.amtDue_tx.Size = new System.Drawing.Size(387, 70);
@@ -79,7 +82,7 @@
             this.pay_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.pay_btn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pay_btn.ForeColor = System.Drawing.Color.White;
-            this.pay_btn.Location = new System.Drawing.Point(229, 627);
+            this.pay_btn.Location = new System.Drawing.Point(229, 603);
             this.pay_btn.Margin = new System.Windows.Forms.Padding(4);
             this.pay_btn.Name = "pay_btn";
             this.pay_btn.Size = new System.Drawing.Size(187, 42);
@@ -91,7 +94,7 @@
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.White;
-            this.panel5.Location = new System.Drawing.Point(31, 277);
+            this.panel5.Location = new System.Drawing.Point(31, 253);
             this.panel5.Margin = new System.Windows.Forms.Padding(4);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(27, 63);
@@ -99,8 +102,9 @@
             // 
             // cash_tx
             // 
+            this.cash_tx.DecimalPlaces = 2;
             this.cash_tx.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cash_tx.Location = new System.Drawing.Point(29, 275);
+            this.cash_tx.Location = new System.Drawing.Point(29, 251);
             this.cash_tx.Margin = new System.Windows.Forms.Padding(4);
             this.cash_tx.Maximum = new decimal(new int[] {
             100000000,
@@ -115,11 +119,12 @@
             this.cash_tx.ThousandsSeparator = true;
             this.cash_tx.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
             this.cash_tx.ValueChanged += new System.EventHandler(this.cash_tx_ValueChanged);
+            this.cash_tx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cash_tx_KeyPress);
             // 
             // title_lbl
             // 
             this.title_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title_lbl.Location = new System.Drawing.Point(29, 108);
+            this.title_lbl.Location = new System.Drawing.Point(29, 74);
             this.title_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.title_lbl.Name = "title_lbl";
             this.title_lbl.Size = new System.Drawing.Size(387, 35);
@@ -131,7 +136,7 @@
             // 
             this.product_tx.AutoSize = true;
             this.product_tx.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.product_tx.Location = new System.Drawing.Point(26, 245);
+            this.product_tx.Location = new System.Drawing.Point(26, 221);
             this.product_tx.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.product_tx.Name = "product_tx";
             this.product_tx.Size = new System.Drawing.Size(138, 28);
@@ -141,7 +146,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(187, 35);
+            this.pictureBox1.Location = new System.Drawing.Point(187, 11);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(70, 70);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -152,84 +157,128 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 370);
+            this.label1.Location = new System.Drawing.Point(29, 346);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 20);
             this.label1.TabIndex = 544;
             this.label1.Text = "Discount Rate";
             // 
-            // numericUpDown1
+            // discRate_tx
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(29, 392);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(387, 43);
-            this.numericUpDown1.TabIndex = 542;
-            this.numericUpDown1.Tag = "IsNumeric";
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.discRate_tx.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discRate_tx.Location = new System.Drawing.Point(29, 368);
+            this.discRate_tx.Margin = new System.Windows.Forms.Padding(4);
+            this.discRate_tx.Name = "discRate_tx";
+            this.discRate_tx.Size = new System.Drawing.Size(387, 43);
+            this.discRate_tx.TabIndex = 542;
+            this.discRate_tx.Tag = "IsNumeric";
+            this.discRate_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.discRate_tx.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.discRate_tx.ValueChanged += new System.EventHandler(this.discRate_tx_ValueChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 447);
+            this.label2.Location = new System.Drawing.Point(29, 423);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(124, 20);
             this.label2.TabIndex = 547;
             this.label2.Text = "Discount Amount";
             // 
-            // numericUpDown2
+            // discAmt_tx
             // 
-            this.numericUpDown2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown2.Location = new System.Drawing.Point(29, 469);
-            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
-            this.numericUpDown2.Maximum = new decimal(new int[] {
+            this.discAmt_tx.BackColor = System.Drawing.Color.White;
+            this.discAmt_tx.DecimalPlaces = 2;
+            this.discAmt_tx.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discAmt_tx.Location = new System.Drawing.Point(29, 445);
+            this.discAmt_tx.Margin = new System.Windows.Forms.Padding(4);
+            this.discAmt_tx.Maximum = new decimal(new int[] {
             100000000,
             0,
             0,
             0});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(387, 43);
-            this.numericUpDown2.TabIndex = 545;
-            this.numericUpDown2.Tag = "IsNumeric";
-            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown2.ThousandsSeparator = true;
-            this.numericUpDown2.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.discAmt_tx.Minimum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            -2147483648});
+            this.discAmt_tx.Name = "discAmt_tx";
+            this.discAmt_tx.ReadOnly = true;
+            this.discAmt_tx.Size = new System.Drawing.Size(387, 43);
+            this.discAmt_tx.TabIndex = 545;
+            this.discAmt_tx.Tag = "IsNumeric";
+            this.discAmt_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.discAmt_tx.ThousandsSeparator = true;
+            this.discAmt_tx.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.discAmt_tx.ValueChanged += new System.EventHandler(this.discAmt_tx_ValueChanged);
             // 
-            // numericUpDown3
+            // change_tx
             // 
-            this.numericUpDown3.BackColor = System.Drawing.Color.White;
-            this.numericUpDown3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown3.Location = new System.Drawing.Point(29, 542);
-            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4);
-            this.numericUpDown3.Maximum = new decimal(new int[] {
+            this.change_tx.BackColor = System.Drawing.Color.White;
+            this.change_tx.DecimalPlaces = 2;
+            this.change_tx.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.change_tx.Location = new System.Drawing.Point(29, 518);
+            this.change_tx.Margin = new System.Windows.Forms.Padding(4);
+            this.change_tx.Maximum = new decimal(new int[] {
             100000000,
             0,
             0,
             0});
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.ReadOnly = true;
-            this.numericUpDown3.Size = new System.Drawing.Size(387, 43);
-            this.numericUpDown3.TabIndex = 548;
-            this.numericUpDown3.Tag = "IsNumeric";
-            this.numericUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown3.ThousandsSeparator = true;
-            this.numericUpDown3.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.change_tx.Minimum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            -2147483648});
+            this.change_tx.Name = "change_tx";
+            this.change_tx.ReadOnly = true;
+            this.change_tx.Size = new System.Drawing.Size(387, 43);
+            this.change_tx.TabIndex = 548;
+            this.change_tx.Tag = "IsNumeric";
+            this.change_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.change_tx.ThousandsSeparator = true;
+            this.change_tx.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.change_tx.ValueChanged += new System.EventHandler(this.change_tx_ValueChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 520);
+            this.label3.Location = new System.Drawing.Point(29, 496);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 20);
             this.label3.TabIndex = 549;
             this.label3.Text = "Change";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(32, 370);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(17, 40);
+            this.panel1.TabIndex = 550;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(31, 446);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(17, 40);
+            this.panel2.TabIndex = 551;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(31, 519);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(17, 40);
+            this.panel3.TabIndex = 552;
             // 
             // Pay
             // 
@@ -237,20 +286,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(442, 695);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.product_tx);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.customer_tx);
             this.Controls.Add(this.amtDue_tx);
+            this.Controls.Add(this.title_lbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.change_tx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.discAmt_tx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.discRate_tx);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.product_tx);
             this.Controls.Add(this.pay_btn);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.cash_tx);
-            this.Controls.Add(this.title_lbl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -262,9 +314,9 @@
             this.Load += new System.EventHandler(this.PayForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cash_tx)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discRate_tx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discAmt_tx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.change_tx)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -281,10 +333,13 @@
         internal System.Windows.Forms.Label product_tx;
         private System.Windows.Forms.PictureBox pictureBox1;
         internal System.Windows.Forms.Label label1;
-        public System.Windows.Forms.NumericUpDown numericUpDown1;
+        public System.Windows.Forms.NumericUpDown discRate_tx;
         internal System.Windows.Forms.Label label2;
-        public System.Windows.Forms.NumericUpDown numericUpDown2;
-        public System.Windows.Forms.NumericUpDown numericUpDown3;
+        public System.Windows.Forms.NumericUpDown discAmt_tx;
+        public System.Windows.Forms.NumericUpDown change_tx;
         internal System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- Forms/Orders/PayForm.cs
+++ Forms/Orders/PayForm.cs
@@ -1,4 +1,5 @@
 ﻿using SalesPro.Helpers;
+using SalesPro.Models;
 using SalesPro.Services;
 using System;
 using System.Windows.Forms;
@@ -9,7 +10,9 @@ namespace SalesPro.Forms.Orders
     {
         public int _orderId;
         public int _rowVersion;
+        private decimal _amountDue;
         private DateTime _curDate;
+
         private readonly OrderService _service;
         public Pay()
         {
@@ -21,13 +24,57 @@ namespace SalesPro.Forms.Orders
         {
             try
             {
+                var cash = cash_tx.Value;
+                if (cash == 0)
+                {
+                    MessageHandler.ShowWarning("Please enter cash amount. Amount cannot be 0");
+                    return;
+                }
+                if (cash < _amountDue)
+                {
+                    MessageHandler.ShowWarning("Cash amount is less than the amount due");
+                    return;
+                }
                 await _service.PayOrder(_orderId, decimal.Parse(cash_tx.Text), _curDate, _rowVersion);
                 MessageHandler.ShowInfo("Order paid successfully");
+                Close();
             }
             catch (Exception ex)
             {
                 MessageHandler.ShowError($"Error pay button click: {ex.Message}");
             }
+        }
+
+        private OrderModel CalculateOrderPayment(decimal total)
+        {
+            var orderModel = new OrderModel();
+            try
+            {
+                decimal cashTendered = cash_tx.Value;
+                int discountRate = (int)discRate_tx.Value;
+                // Calculate the discount amount
+                decimal discountAmount = (total * discountRate) / 100;
+
+                // Calculate the final total after discount
+                decimal finalTotal = total - discountAmount;
+
+                // Calculate the change
+                decimal change = cashTendered - finalTotal;
+
+                discAmt_tx.Value = discountAmount;
+                change_tx.Value = change;
+
+                // model
+                orderModel.AmountPaid = cashTendered;
+                orderModel.DiscountRate = discountRate;
+                orderModel.DiscountAmount = discountAmount;
+                orderModel.Change = change;
+            }
+            catch (Exception ex)
+            {
+                MessageHandler.ShowError($"Error calculating order payment: {ex.Message}");
+            }
+            return orderModel;
         }
 
         private async void PayForm_Load(object sender, EventArgs e)
@@ -39,6 +86,7 @@ namespace SalesPro.Forms.Orders
                 if (order != null)
                 {
                     amtDue_tx.Text = $"₱{order.AmountDue.ToString()}";
+                    _amountDue = order.AmountDue;
                     customer_tx.Text = order.CustomerName;
                 }
             }
@@ -49,6 +97,26 @@ namespace SalesPro.Forms.Orders
         }
 
         private void cash_tx_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateOrderPayment(_amountDue);
+        }
+
+        private void cash_tx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // CalculateOrderPayment(_amountDue);
+        }
+
+        private void discRate_tx_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateOrderPayment(_amountDue);
+        }
+
+        private void discAmt_tx_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void change_tx_ValueChanged(object sender, EventArgs e)
         {
 
         }
