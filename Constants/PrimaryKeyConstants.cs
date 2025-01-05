53cc26b25b959d105f89bd86d724f4a5693d8c94
jagwit <115094@nelnet.net>
2025-01-05 20:45:49 +0800

    Added primary

---
+++ Constants/PrimaryKeyConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace SalesPro.Constants
+{
+    public class PrimaryKeyConstants
+    {
+        public const string BankId = "BankId";
+        public const string CustomerCreditPaymentId = "CustomerCreditPaymentId";
+        public const string CustomerCreditId = "CustomerCreditId";
+        public const string CustomerId = "CustomerId";
+        public const string ExpenseId = "ExpenseId";
+        public const string InventoryId = "InventoryId";
+        public const string InventoryLogId = "InventoryLogId";
+        public const string OrderItemId = "OrderItemId";
+        public const string OrderId = "OrderId";
+        public const string ProductId = "ProductId";
+        public const string PurchaseOrderItemId = "PurchaseOrderItemId";
+        public const string PurchaseOrderLogsId = "PurchaseOrderLogsId";
+        public const string PurchaseOrderPaymentId = "PurchaseOrderPaymentId";
+        public const string PurchaseOrderId = "PurchaseOrderId";
+        public const string SupplierId = "SupplierId";
+        public const string TransactionId = "TransactionId";
+        public const string TransactionLogId = "TransactionLogId";
+        public const string UserId = "UserId";
+    }
+}
